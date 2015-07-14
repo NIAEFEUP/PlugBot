0363005c4b91a7e4952450cbff813a67bfbe645c
Diogo Basto <diogotbasto@gmail.com>
2015-07-15 00:06:16 +0100

    Building blocks are there, command handling/parsing not working

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ var plugAPI = require('plugapi');
 var fs = require('fs');
 var path = require('path');
 
+var commands = require(path.resolve(__dirname, 'commands.js'));
 var config = require(path.resolve(__dirname, 'config.json'));
+var reconnectionAttempts=0;
 
 bot = new plugAPI(config.auth);
 
@@ -10,26 +12,70 @@ bot.connect(config.roomName);
 logger = plugAPI.CreateLogger('Bot');
 
 bot.on('roomJoin',function(room){
-    logger.success("Joined room",room)
-    //bot.log("Connected to "+room);
-    //bot.sendChat("Hello. This is a test");
-
+    logger.success("Joined room",room);
+    reconnectionAttempts=0;
 });
 
 bot.on("chat",function(data){
-
-   logger.info("chat:",JSON.stringify(data),":",data.message);
-    if (data.raw.un!== "Castigador da Parvoice") bot.sendChat("ola");
-    //bot.disconnect();
+   //logger.info("chat:",JSON.stringify(data),":",data.message);
+    //chat: {"raw":{"cid":"3823277-1436894615110","message":"ola","sub":0,"uid":3823277,"un":"Rue Stephane"},"id":"3823277-1436894615110","from":{"avatarID":"island-s04","badge":"isle08","gRole":0,"grab":false,"id":3823277,"joined":"2013-05-27 23:52:31.624000","language":null,"level":8,"role":3,"slug":null,"status":1,"sub":0,"username":"Rue Stephane","vote":1},"message":"ola","mentions":[],"muted":false,"type":"message"} : ola
+    //chat: {"raw":{"cid":"8266958-1436894615305","message":"ola","sub":0,"uid":8266958,"un":"Castigador da Parvoice"},"id":"8266958-1436894615305","from":{"avatarID":"base07","badge":null,"gRole":0,"grab":false,"id":8266958,"ignores":[],"joined":"2015-07-14 13:38:41.243226","language":"en","level":3,"notifications":[{"action":"levelUp","id":15762304,"timestamp":"2015-07-14 14:00:53.805598","value":"3"},{"action":"levelUp","id":15762071,"timestamp":"2015-07-14 13:43:41.379572","value":"2"}],"pp":1638,"pw":true,"role":0,"slug":"castigador-da-parvoice","status":1,"sub":0,"username":"Castigador da Parvoice","vote":0,"xp":122},"message":"ola","mentions":[],"muted":false,"type":"message"} : ola
+    try{
+        if (data.message.charAt(0)==='.')
+        {
+            logger.info("parsing command ",data.raw.un,":",data.message);
+            commands.handle(data);
+        }
+    }
+    catch (e)
+    {
+        logger.error("exception in chat event:",e);
+    }
 });
-//console.log("Connected to ",config.roomName);
-/*bot.on('close', reconnect);
+
+bot.on("chat:mention",function(data){
+    try{
+        commands.replyMention(data);
+    }
+    catch (e)
+    {
+        logger.error("exception in chat_mention event:",e);
+    }
+});
+
+bot.on("userJoin",function(data){
+    //User.join(data);
+   try{
+       commands.greet(data);
+   }
+   catch (e)
+   {
+       logger.error("exception in userJoin event:",e);
+   }
+});
+
+bot.on('close', reconnect);
+
 bot.on('error', reconnect);
 
+
+
 function reconnect() {
-    console.log("reconnecting");
-    bot.connect(config.roomName);
+
+    if (reconnectionAttempts<config.maxReconnectionAttempts)
+    {
+        reconnectionAttempts+=1;
+        logger.error("reconnecting ",reconnectionAttempts);
+        bot.connect(config.roomName);
+    }
+    else
+    {
+        logger.error("Failed to connect, exiting");
+        process.exit(2);
+    }
 }
-*/
+
+
+
 
 
---
+++ commands.js
@@ -0,0 +1,60 @@
+exports.handle = handle;
+exports.replyMention = replyMention;
+exports.greet = greet;
+
+function handle(data)
+{
+    for (var i=0;i<commands.length;i++)
+    {
+        if (data.message.slice(commands[i].name.length) === commands[i].name) {
+            //missing permission level check
+            commands[i].handler(data);
+            break;
+        }
+    }
+}
+
+function replyMention(data)
+{
+    //basic test, want to add some conversation module
+    var msg=data.message;
+    if (msg.search("fish")!=-1)
+        bot.sendChat("fish go m00");
+    else if (msg.search("cows")!=-1)
+        bot.sendChat("cows go blub");
+    else bot.sendChat("Sup @"+data.raw.un);
+}
+
+function greet(data)
+{
+    bot.sendChat("Olá @"+data.username+"!");
+}
+
+// command levels: 1-user,2-dj,3-bouncer,4-manager,5-host
+
+//add user to queue
+var _add={
+    "name":".add",
+    "level":3,
+    "handler":function(data){
+        console.log("test add");
+
+    }
+};
+
+var _help={
+    "name":".help",
+    "level":1,
+    "handler":function(data){
+        console.log("test help");
+        var str="";
+        for (var i=0;i<commands.length;i++)
+        {
+            if (commands[i].level==1) //only show user commands, mods have to know their shit
+                str+=commands[i].name+" ";
+        }
+        bot.sendChat("Available commands: "+str);
+    }
+};
+
+var commands=[_add,_help];
