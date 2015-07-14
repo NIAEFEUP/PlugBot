9dfbd74070c1e1977cd1881dde179be3e6808046
Diogo Basto <diogotbasto@gmail.com>
2015-07-14 18:24:21 +0100

    Connecting and replying to chat

--- bot.js
+++ bot.js
@@ -7,11 +7,20 @@ var config = require(path.resolve(__dirname, 'config.json'));
 bot = new plugAPI(config.auth);
 
 bot.connect(config.roomName);
+logger = plugAPI.CreateLogger('Bot');
 
 bot.on('roomJoin',function(room){
-    bot.log("Connected to "+room);
-    bot.sendChat("Hello. This is a test @Rue Stephane");
-    //bot.disconnect()
+    logger.success("Joined room",room)
+    //bot.log("Connected to "+room);
+    //bot.sendChat("Hello. This is a test");
+
+});
+
+bot.on("chat",function(data){
+
+   logger.info("chat:",JSON.stringify(data),":",data.message);
+    if (data.raw.un!== "Castigador da Parvoice") bot.sendChat("ola");
+    //bot.disconnect();
 });
 //console.log("Connected to ",config.roomName);
 /*bot.on('close', reconnect);
